1fb759d74593cb72b86d4c8c5199bdfd67ef07c6
Thefirstxerg <thefirstxerg@gmail.com>
2025-03-24 13:57:20 +0200

    Update

--- Code/Chapter1/server.js
+++ Code/Chapter1/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+
 const app = express();
 // Set static folder
 app.use(express.static('public'));
@@ -6,7 +7,27 @@ app.use(express.static('public'));
 app.use(express.urlencoded ({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
+
+// Handle GET request to fetch users
+app.get('/users', async (req, res) => {
+    setTimeout(async ()=> {
+    const limit = +req.query.limit || 10;
+    const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
+    );
+    const users = await response.json()
+    res.send(`
+    <h2>Users</h2>
+    <ul class="list-group">
+    ${users.map((user)=>`<li class="list-group-item">${user.name}</li>`).join('')}
+    </ul>
+    `)
+    },2000)
+   });
+   
+
 // Start the server
 app.listen (3000, ()=>{
-console. log('Server listening on port 3000');
+    console.log('Server listening on port 3000');
 });
+
