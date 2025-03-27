483531a74d7d520f81d9a2b3bbe21888df9e77a3
Thefirstxerg <thefirstxerg@gmail.com>
2025-03-27 09:30:31 +0200

    Upload

--- Code/Chapter5/server.js
+++ Code/Chapter5/server.js
@@ -14,40 +14,40 @@ app.use(express.json());
 app.post('/email', (req, res) => {
     const submittedEmail = req.body.email;
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if(emailRegex.test(submittedEmail)) {
-        return res.send(`
-        <div class="mb-3" hx-target="this" hx-swap="outerHTML">
-        <label class="form-label">Email address</label>
-        <input
-        type="email"
-        class="form-control"
-        name="email"
-        hx-post="/email"
-        value="${submittedEmail}"
-        >
-        <div class="alert alert-success" role="alert">
-       That email is valid
-        </div>
-        </div>`
-        )
-    } else {
-        return res.send(`
-        <div class="mb-3" hx-target="this" hx-swap="outerHTML">
-        <label class="form-label">Email address</label>
-        <input
-        type="email"
-        class="form-control"
-        name="email"
-        hx-post="/email"
-        value="${submittedEmail}"
-        >
-        <div class="alert alert-danger" role="alert">
-        Please enter a valid email address
-        </div>
-        </div>`
-        )
-    }
-});
+        if(emailRegex.test(submittedEmail)) {
+            return res.send(`
+                <div class="mb-3" hx-target="this" hx-swap="outerHTML">
+                    <label class="form-label">Email address</label>
+                    <input
+                        type="email"
+                        class="form-control"
+                        name="email"
+                        hx-post="/email"
+                        value="${submittedEmail}"
+                        >
+                    <div class="alert alert-success" role="alert">
+                    That email is valid
+                    </div>
+                </div>`
+            );
+        } else {
+            return res.send(`
+                <div class="mb-3" hx-target="this" hx-swap="outerHTML">
+                    <label class="form-label">Email address</label>
+                    <input
+                        type="email"
+                        class="form-control"
+                        name="email"
+                        hx-post="/email"
+                        value="${submittedEmail}"
+                        >
+                    <div class="alert alert-danger" role="alert">
+                    Please enter a valid email address
+                    </div>
+                </div>`
+            );
+        }
+    });
        
 
 
